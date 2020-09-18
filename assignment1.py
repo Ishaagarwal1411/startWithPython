3801542fc67c76263d224c13935ffe79d59b10db
Ishaagarwal1411 <71475005+Ishaagarwal1411@users.noreply.github.com>
2020-09-18 06:54:01 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(2000,3200):
+    if(i%7==0 and i%5!=0)
+    print(",".join(i))
+
+
+# In[2]:
+
+
+for i in range(2000,3200):
+    if(i%7==0)
+    print(",".join(i))
+
+
+# In[3]:
+
+
+for i in range(2000,3200):
+    if(i%7==0 and i%5!=0):
+    print(i,sep=", ")
+
+
+# In[4]:
+
+
+for i in range(2000,3200):
+    if(i%7==0 and i%5!=0):
+    print(i,sep=", ")
+
+
+# In[5]:
+
+
+for i in range(2000,3200):
+    if(i%7==0 and i%5!=0):
+        print(i,sep=", ")
+
+
+# In[7]:
+
+
+for i in range(2000,3200):
+    if(i%7==0 and i%5!=0):
+        print(i,end=", ")
+
+
+# In[8]:
+
+
+for i in range(2000,3200):
+    if(i%7==0 and i%5!=0):
+        print(",".join(i))
+
+
+# In[9]:
+
+
+items=[]
+for i in range(2000,3200):
+    s=str(i)
+    if(i%7==0 and i%5!=0):
+        items.append(s)
+print(",".join(items))
+
+
+# In[10]:
+
+
+fname=input("Enter your first name: ")
+lname=input("Enter your last name: ")
+print("Hello " + lname + " " + fname )
+
+
+# In[12]:
+
+
+pi=3.14
+r=12
+v=4/3*pi*r**3
+print(v)
+
+
+# In[ ]:
+
+
+
+
